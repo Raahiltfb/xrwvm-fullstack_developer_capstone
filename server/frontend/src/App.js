ef4a99985f94434144eee52821e3dd192b384edf
Raahiltfb <raahil.mehta003@gmail.com>
2025-11-01 07:40:12 -0400

    Module 4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,15 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
-import Register from "./components/Register/Register"; // ✅ Import the Register component
+import Register from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers"; 
+import Dealer from "./components/Dealers/Dealer";    
 
 function App() {
   return (
     <Routes>
-      {/* ✅ Route for Login */}
+      {/*  Route for Login */}
       <Route path="/login" element={<LoginPanel />} />
 
-      {/* ✅ Route for Register */}
+      {/*  Route for Register */}
       <Route path="/register" element={<Register />} />
+
+      {/*  Route for Dealers */}
+      <Route path="/dealers" element={<Dealers />} />
+
+      {/* ✅ Route for individual Dealer page */}
+      <Route path="/dealer/:id" element={<Dealer />} />
     </Routes>
   );
 }
